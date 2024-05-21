d0feb4ba97a4ee0bc6fdb40d1c59bc75eb516185
Fredamian <fredybrito03@gmail.com>
2024-05-21 12:13:52 -0100

    .

---
+++ controllers/admin.js
@@ -0,0 +1,9 @@
+const AddPerson = require('../models/product');
+
+exports.getAddPerson = (req, res, next) => {
+  res.status(200).json({editing: false})
+};
+
+exports.postAddProduct = (req, res, next) => {
+  
+};
